56f85eeb33fcd593e72c79277d25be48f072d759
Alaa Desouky <alaa.desouky33@gmail.com>
2018-09-12 20:40:22 +0200

    Handled auth

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Load Routes
+const auth = require("./routes/auth");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
+
+// Use Routes
+app.use("/auth", auth);
 
 // Server Config
 const port = process.env.PORT || 5000;
---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb://alaa:desouky123654@ds155252.mlab.com:55252/storybooks-dev_desouky",
+  googleClientID:
+    "750863185674-r3oivrnicuqngpeh6bhsu9gh6e014u9d.apps.googleusercontent.com",
+  googleClientSecret: "xn7tMCadyx8rgVCG5bokESpf"
+};
