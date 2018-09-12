f6f7d9c73a8fcd0619fd7cc7ab9a14cec259fd44
Alaa Desouky <alaa.desouky33@gmail.com>
2018-09-12 19:37:33 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+// Server Config
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
