47b7877109c507da04e0dbddd3070547bddd442b
Alaa Desouky <alaa.desouky33@gmail.com>
2018-09-13 07:55:34 +0200

    Added header and navbar

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
@@ -11,6 +12,7 @@ require("./models/User");
 require("./config/passport")(passport);
 
 // Load Routes
+const index = require("./routes/index");
 const auth = require("./routes/auth");
 
 // Load Keys
@@ -26,9 +28,14 @@ mongoose
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome");
-});
+// Handlebars Middleware
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
 // Cookie Parser and Express Session Config
 app.use(cookieParser());
@@ -51,6 +58,7 @@ app.use((req, res, next) => {
 });
 
 // Use Routes
+app.use("/", index);
 app.use("/auth", auth);
 
 // Server Config
