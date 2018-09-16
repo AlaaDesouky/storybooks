71610e04ae1b46a791e1ed8992823cdb093452ec
Alaa Desouky <alaa.desouky33@gmail.com>
2018-09-16 15:54:55 +0200

    version 1.0.0alpha done

---
+++ config/key_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: process.env.MONGOURI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+};
--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://alaa:desouky123654@ds155252.mlab.com:55252/storybooks-dev_desouky",
-  googleClientID:
-    "750863185674-r3oivrnicuqngpeh6bhsu9gh6e014u9d.apps.googleusercontent.com",
-  googleClientSecret: "xn7tMCadyx8rgVCG5bokESpf"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./key_prod");
+} else {
+  module.exports = require("./key_dev");
+}
